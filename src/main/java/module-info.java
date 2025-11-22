b3c642145a8a5294f34bb6956dfb20a930f98de8
Игорь Аведисьян <guffy911d@gmail.com>
2025-11-22 17:14:51 +0300

    Create project

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module org.example.ez_ide {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens org.example.ez_ide to javafx.fxml;
+    exports org.example.ez_ide;
+}
